4bfef2b4d8467f83c5e0ff01b2b477073247872e
xianghang mi <xmi@iu.edu>
2016-02-19 11:47:57 -0500

    fix

--- emulateHttp2/testByBrowser.sh
+++ emulateHttp2/testByBrowser.sh
@@ -17,6 +17,7 @@ cancelDownload(){
 
 startSendRequest() {
   requestUrl=$1
+  echo $1
   wndId=$(xdotool search --name "Google Chrome")
   if [ "$wndId" == "" ];then
 	google-chrome >/dev/null 2>&1 &
@@ -38,18 +39,74 @@ killBrowser() {
   fi
   xdotool windowkill $wndId
 }
+requestAndSniff() {
+	if [ $# -lt 2 ];then
+	  echo "Usage requestUrl logFile\n"
+	fi
+	sudo ./packetSniff $device "$filterExpression" payloadFile $2 >logSniff &
+	sniffId=$!
+	echo "processId of sniff is $sniffId"
+	startSendRequest $1
+	sleep 10
+	cancelDownload
+	sudo kill -SIGCONT $sniffId
+	sleep 5
+	sudo kill -SIGINT $sniffId
+	killBrowser
+}
+curDir=$(pwd)
+ip="156.56.83.24"
+portH2=6003
+portH1=6004
+urlH1="https://http1_test:6004"
+urlH2="https://http2_test:6003"
+fileName="250mb.tar.gz"
+num=2
+device="eth0"
+filterExpression="src host $ip"
+resultDir=$curDir/testWithBrowser_$(date +"%Y%m%d_%H%M%S")
+mkdir -p $resultDir
+delays=("50ms" "100ms" "250ms" "500ms")
+bdlimits=("100mbit" "50mbit" "10mbit" "5mbit")
+bufferSize=(1048576 2097152 4194304 8388608 16777216)
 
-if [ $# -lt 1 ];then
-  echo "Usage requestUrl"
-  exit 1
-fi
-sudo ./packetSniff eth0 "src host 156.56.83.24" payloadFile logFile >logSniff &
-sniffId=$!
-echo "processId of sniff is $sniffId"
-startSendRequest $1
-sleep 10
-cancelDownload
-sudo kill -SIGCONT $sniffId
-sleep 5
-sudo kill -SIGINT $sniffId
-killBrowser
+defaultSize=524288 
+for defaultSize in ${bufferSize[@]}:
+do
+  minSize=$((defaultSize / 2))
+  maxSize=$((defaultSize * 2))
+  echo "change tcp buffer to $minSize, $defaultSize, $maxSize"
+  $curDir/interaction.exp "sudo ./tuneTcp.sh $minSize $defaultSize $maxSize;sudo sysctl -p;sudo sysctl net.ipv4.tcp_wmem"
+  if [ $? -ne 0 ];then
+	echo "set tcp buffer failed"
+	exit 1
+  fi
+  date=$( date +"%Y%m%d-%H%M")
+
+  delay="0ms"
+  bdlimit="100mbit"
+  for delay in ${delays[@]}
+  do
+	  for bdlimit in ${bdlimits[@]}
+	  do
+		  echo "bandwidth limitation: $bdlimit, delay: $delay"
+		  $curDir/interaction.exp "sudo ./setBdLimit.sh $bdlimit $portH2 $delay"
+		  i=0
+		  while [ $i -lt $num ]
+		  do
+			requestAndSniff $urlH2/$fileName $resultDir/sniff_H2_${bdlimit}_${delay}_${defaultSize}
+			((i++))
+		  done
+
+		  $curDir/interaction.exp "sudo ./setBdLimit.sh $bdlimit $portH1 $delay"
+		  i=0
+		  while [ $i -lt $num ]
+		  do
+			requestAndSniff $urlH1/$fileName $resultDir/sniff_H1_${bdlimit}_${delay}_${defaultSize}
+			((i++))
+		  done
+	  done
+  done
+
+done
+
